a21ac7a812bd83ad032f50a08341e4d84e119ad6
josia <josia@DESKTOP-CE2G2G9>
2020-10-06 22:46:32 -0700

    adding javadoc comments

--- cse360/src/cse360assignment02/AddingMachine.java
+++ cse360/src/cse360assignment02/AddingMachine.java
@@ -1,25 +1,56 @@
 package cse360assignment02;
-
+/**
+ * This class implements an adding machine
+ * @author josia
+ *Version 1.0
+ */
 public class AddingMachine {
   private int total;
+  
+  /**
+   * This begins the adding machine
+   */
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
+  /**
+   * This returns the total of the summation
+   * @return returns the total
+   */
+  
   public int getTotal () {
     return 0;
   }
   
+  /**
+   * This adds a value to the summation
+   * @param value the value to be added
+   */
+  
   public void add (int value) {
   }
+  
+  /**
+   *  This subtracts a value from the summation
+   * @param value the value to be subtracted
+   */
 
   public void subtract (int value) {
   }
+  
+  /**
+   * Returns the string
+   */
 
   public String toString () {
     return "";
   }
+  
+  /**
+   * Clears the adding machine
+   */
 
   public void clear() {
   }
